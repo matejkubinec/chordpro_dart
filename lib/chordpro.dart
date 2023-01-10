73a79a083e79a9635979fa5dd8db847f92477b09
Matej Kubinec <matej.kubinec@outlook.com>
2023-01-10 15:07:50 +0100

    feat: export additional members

---
+++ lib/chordpro.dart
@@ -0,0 +1,8 @@
+library chordpro;
+
+export 'src/song.dart';
+export 'src/block.dart';
+export 'src/line.dart';
+export 'src/metadata.dart';
+export 'src/section.dart';
+export 'src/utils.dart';
--- lib/song.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library chordpro;
-
-export 'src/song.dart';
-
-// TODO: Export any libraries intended for clients of this package.
